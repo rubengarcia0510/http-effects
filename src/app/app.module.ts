53dcac0a988ecffe4074c178907d7bfeb0d4f173
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-22 19:03:24 -0300

    usuarios service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
+
 
 @NgModule({
   declarations: [
@@ -12,6 +14,7 @@ import { UsuariosModule } from './usuarios/usuarios.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     SharedModule,
     UsuariosModule
