0b29a8cfcaf686a4da1667f7b63e436d06ffda7b
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-22 19:03:12 -0300

    html lista usuarios

--- src/app/usuarios/lista-usuarios/lista-usuarios.component.ts
+++ src/app/usuarios/lista-usuarios/lista-usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-lista-usuarios',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaUsuariosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UsuariosService) { }
 
   ngOnInit(): void {
+    this.userService.getUsers()
+        .subscribe(data=>{
+          console.log(data)
+        })
   }
 
 }
