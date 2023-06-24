54dbde2b1abe3f4a8afe445174776ecedb9adb3a
Ruben Garcia <garcia.r@opendevpro.com>
2023-06-24 19:20:00 -0300

    show user

--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { Usuario } from 'src/app/models/usuario.model';
 import { loadUser } from 'src/app/store/actions';
 import { AppState } from 'src/app/store/app.reducers';
 
@@ -11,11 +12,16 @@ import { AppState } from 'src/app/store/app.reducers';
 })
 export class UsuarioComponent implements OnInit {
 
+  usuario: Usuario
+
   constructor(private route: ActivatedRoute,
               private store: Store<AppState>
     ) { }
 
   ngOnInit(): void {
+
+    this.store.select('user')
+          .subscribe(user=>this.usuario = user.user)
 
     this.route.params.subscribe(data => 
           this.store.dispatch(loadUser({id:data.id}))
